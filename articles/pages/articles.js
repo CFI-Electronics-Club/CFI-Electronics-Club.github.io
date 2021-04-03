2bfdfc0acd4f20b8ed6619d1858317d55a47ab5b
Dhinesh <64545984+BalaDhinesh@users.noreply.github.com>
2021-04-03 17:27:35 +0530

    Add files via upload

--- articles/pages/articles.js
+++ articles/pages/articles.js
@@ -1,15 +1,12 @@
+// function to set a given theme/color-scheme
 function setTheme(themeName) {
   localStorage.setItem("theme", themeName);
   if (themeName == "theme-dark") {
     document.styleSheets[0].disabled = true;
     document.styleSheets[1].disabled = false;
-    document.getElementById("nav").style.background = "#363636";
-
   } else {
     document.styleSheets[0].disabled = false;
     document.styleSheets[1].disabled = true;
-    document.getElementById("nav").style.background = "midnightblue";
-
   }
 }
 // function to toggle between light and dark theme
@@ -25,11 +22,11 @@ function toggleTheme() {
 (function () {
   var check = document.getElementById("checkbox");
 
-  if (localStorage.getItem("theme") === "theme-light") {
-    setTheme("theme-light");
-    check.checked = false;
-  } else {
+  if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
     check.checked = true;
+  } else {
+    setTheme("theme-light");
+    check.checked = false;
   }
 })();
